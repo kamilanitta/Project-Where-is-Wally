1b7560a53a80cbc555fcc01e46b03a4601108d0b
Kamila Nitta <kamila.nitta@gmail.com>
2021-06-15 23:40:17 +0200

    update JS

--- js/classes.js
+++ js/classes.js
@@ -1,19 +1,68 @@
-class FindWally{
-    constructor(){
-        this.points=0;
-        this.timers = 10;
-        this.wally = ();
+class FindWally {
+  constructor() {
+    this.points = 0;
+    this.timers = 0;
+    this.wally = 0;
+  }
+  getPoints() {
+    if (this.wally !== 0) {
+      this.points += 1;
     }
+  }
+
+  isTimeOver() {
+    if (this.timers === 0) {
+      return "Game Over!";
+    }
+  }
 }
 
-gainPoints(){
-    if(this.wally !==0){
-        this.points += 1
+class Timer {
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = null;
+  }
+
+  start(callback) {
+    return (this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      callback();
+    }, 1000));
+  }
+
+  getMinutes() {
+    return parseInt(this.currentTime / 60);
+  }
+
+  getSeconds() {
+    return parseInt(this.currentTime % 60);
+  }
+
+  computeTwoDigitNumber(value) {
+    if (value < 10) {
+      return `0${value}`;
     }
+    return `${value}`;
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+  }
+
+  reset() {
+    this.currentTime = 0;
+  }
+
+  split() {
+    const min = this.computeTwoDigitNumber(this.getMinutes());
+    const sec = this.computeTwoDigitNumber(this.getSeconds());
+
+    return `${min}:${sec}`;
+  }
 }
 
-isTimeOver(){
-    if(this.timers === 0){
-        return "Game Over!"
-    }
+// The following is required to make unit tests work.
+/* Environment setup. Do not modify the below code. */
+if (typeof module !== "undefined") {
+  module.exports = Timer;
 }
--- js/index.js
+++ js/index.js
@@ -1,13 +1,56 @@
-let playBtnClick = document.getElementsByClassName("playBtn");
+const timer = new Timer();
+
+//get the buttons:
+let playBtnClick = document.getElementById("playBtn");
 let findWally = document.getElementsByClassName("findWally");
 
+// Timer
 
+const minDecElement = document.getElementById("minDec");
+const minUniElement = document.getElementById("minUni");
+const secDecElement = document.getElementById("secDec");
+const secUniElement = document.getElementById("secUni");
 
-function startGame (){
-    playBtnClick.addEventListener ("click", ()=>{
-        this.timer --;
+function printTime() {
+  const sec = printSeconds();
+  const min = printMinutes();
+}
 
-        if()
-    })
-};
+function printMinutes() {
+  const printMin = timer.computeTwoDigitNumber(timer.getMinutes());
+  minUniElement.innerText = printMin[1];
+  minDecElement.innerText = printMin[0];
+}
+
+function printSeconds() {
+  const printSec = timer.computeTwoDigitNumber(timer.getSeconds());
+  secUniElement.innerText = printSec[1];
+  secDecElement.innerText = printSec[0];
+}
+
+//nao pode estar dentro da funcao
+//getElements devolve array;
+const blurArea = document.getElementsByClassName("blur");
+
+playBtnClick.addEventListener("click", () => {
+  blurArea[0].style.display = "none";
+  timer.start(printTime);
+
+  //check why music is not working
+  initialMusic = new sound("./sounds/bensound-littleidea.mp3");
+  initialMusic.play();
+});
+
+function incorrectWally() {
+  this.audioForIncorrect = [{ name: "uhOh", file: "../sounds/Icq_uh_Oh.mp3" }];
+  let audioInc = new Audio();
+  audioInc.play();
+  audioInc.volume = 0.1;
+}
+
+function correctwally() {
+  this.audioForCorrect = [{ name: "foundWally", file: "../sounds/found.mp3" }];
+  var audioCor = new Audio();
+  audioCor.play();
+  audioCor.volume = 0.1;
 }
